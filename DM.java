ca8c834b0e1c527c863985ebdf5bbfbd476c9d41
Michael Mair <s1016269@sms.ed.ac.uk>
2012-03-20 22:56:45 +0000

    Deleted old files, separated new code into two separate classes - DM to handle the MIDI and GUI to handle the... GUI.

--- DM.java
+++ DM.java
@@ -1,16 +1,82 @@
+import java.awt.*;
+import javax.swing.*;
+import javax.sound.midi.*;
+import java.util.*;
+import java.awt.event.*;
+
 public class DM {
-	
-	public static void main(String[] args) {
-		
-		int p[] = {1,0,0,0,1,0,0,0}; // Pattern to play; 1 = ON, 0 = OFF
-		int p1[] = {0,0,1,0,0,0,1,0,0};
-		int p2[] = {1,1,1,1,1,1,1,1};
-		
-		new GUI();
-	    new PlaySound(p, "kick_01.wav"); // Will be moved to fire on button press
-	    new PlaySound(p1, "snare02.wav");
-	    new PlaySound(p2, "H_hat_01.wav");
-		
-	}
-	
-}
+    
+    static Sequencer sequencer;
+    static Sequence sequence;
+    static Track track;
+    static int[] instruments = {35,42,46,38,49,39,50,60,70,72,64,56,58,47,67,63};
+    
+    public static void main (String[] args) {
+        new GUI();
+    }
+        
+    public static void setUpMidi() {
+    	try {
+            sequencer = MidiSystem.getSequencer();
+            sequencer.open();
+            sequence = new Sequence(Sequence.PPQ,4);
+            track = sequence.createTrack();
+            sequencer.setTempoInBPM(120);
+            
+          } catch(Exception e) {e.printStackTrace();}
+    } // close method
+    
+    public static void buildTrackAndStart() {
+    	
+        int[] trackList = null;
+      
+        sequence.deleteTrack(track);
+        track = sequence.createTrack();
+          for (int i = 0; i < 16; i++) {
+            trackList = new int[16];
+   
+            int key = instruments[i];   
+            for (int j = 0; j < 16; j++ ) {         
+                
+                JCheckBox jc = GUI.checkboxList.get(j + 16*i);
+                if ( jc.isSelected()) {
+                   trackList[j] = key;
+                } else {
+                   trackList[j] = 0;
+                }                    
+             } // close inner loop
+     
+        
+             makeTracks(trackList);
+             track.add(makeEvent(176,1,127,0,16));  
+         } // close outer
+         track.add(makeEvent(192,9,1,0,15));      
+         try {
+            sequencer.setSequence(sequence); 
+            sequencer.setLoopCount(sequencer.LOOP_CONTINUOUSLY);                   
+            sequencer.start();
+            sequencer.setTempoInBPM(120);
+         } catch(Exception e) {e.printStackTrace();}
+      } // close buildTrackAndStart method
+              
+      public static void makeTracks(int[] list) {        
+         
+         for (int i = 0; i < 16; i++) {
+            int key = list[i];
+            if (key != 0) {
+               track.add(makeEvent(144,9,key, 100, i));
+               track.add(makeEvent(128,9,key, 100, i+1));
+            }
+         }
+      }
+          
+      public static  MidiEvent makeEvent(int comd, int chan, int one, int two, int tick) {
+          MidiEvent event = null;
+          try {
+              ShortMessage a = new ShortMessage();
+              a.setMessage(comd, chan, one, two);
+              event = new MidiEvent(a, tick);
+          } catch(Exception e) {e.printStackTrace(); }
+          return event;
+      }
+  } // close class
--- GUI.java
+++ GUI.java
@@ -1,14 +1,95 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.Label;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
 import javax.swing.*;
 
 public class GUI extends JFrame {
 	
+	JPanel mainPanel;
+    static ArrayList<JCheckBox> checkboxList;
+    JFrame theFrame;
+    String[] instrumentNames = {"Bass Drum", "Closed Hi-Hat", 
+    	       "Open Hi-Hat","Acoustic Snare", "Crash Cymbal", "Hand Clap", 
+    	       "High Tom", "Hi Bongo", "Maracas", "Whistle", "Low Conga", 
+    	       "Cowbell", "Vibraslap", "Low-mid Tom", "High Agogo", 
+    	       "Open Hi Conga"};
+	
 	public GUI() {
 		
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    this.setTitle("Test Sound Clip");
-	    this.setSize(300, 200);
-	    this.setVisible(true);
+		theFrame = new JFrame("Drum Machine");
+        theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        BorderLayout layout = new BorderLayout();
+        JPanel background = new JPanel(layout);
+        background.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+        checkboxList = new ArrayList<JCheckBox>();
+        Box buttonBox = new Box(BoxLayout.Y_AXIS);
+        JButton start = new JButton("Start");
+        start.addActionListener(new MyStartListener());
+        buttonBox.add(start);         
+          
+        JButton stop = new JButton("Stop");
+        stop.addActionListener(new MyStopListener());
+        buttonBox.add(stop);
+        
+        JButton upTempo = new JButton("Tempo Up");
+        upTempo.addActionListener(new MyUpTempoListener());
+        buttonBox.add(upTempo);
+        
+        JButton downTempo = new JButton("Tempo Down");
+        downTempo.addActionListener(new MyDownTempoListener());
+        buttonBox.add(downTempo);
+        
+        Box nameBox = new Box(BoxLayout.Y_AXIS);
+        for (int i = 0; i < 16; i++) {
+           nameBox.add(new Label(instrumentNames[i]));
+        }
+        
+        background.add(BorderLayout.EAST, buttonBox);
+        background.add(BorderLayout.WEST, nameBox);
+        theFrame.getContentPane().add(background);
+          
+        GridLayout grid = new GridLayout(16,16);
+        grid.setVgap(1);
+        grid.setHgap(2);
+        mainPanel = new JPanel(grid);
+        background.add(BorderLayout.CENTER, mainPanel);
+        for (int i = 0; i < 256; i++) {                    
+            JCheckBox c = new JCheckBox();
+            c.setSelected(false);
+            checkboxList.add(c);
+            mainPanel.add(c);            
+        } // end loop
+        DM.setUpMidi();
+        theFrame.setBounds(50,50,300,300);
+        theFrame.pack();
+        theFrame.setVisible(true);
+    } // close method
 		
-	}
+	public class MyStartListener implements ActionListener {
+        public void actionPerformed(ActionEvent a) {
+            DM.buildTrackAndStart();
+        }
+    } // close inner class
+    public class MyStopListener implements ActionListener {
+        public void actionPerformed(ActionEvent a) {
+            DM.sequencer.stop();
+        }
+    } // close inner class
+    public class MyUpTempoListener implements ActionListener {
+        public void actionPerformed(ActionEvent a) {
+     float tempoFactor = DM.sequencer.getTempoFactor(); 
+            DM.sequencer.setTempoFactor((float)(tempoFactor * 1.03));
+        }
+     } // close inner class
+     public class MyDownTempoListener implements ActionListener {
+         public void actionPerformed(ActionEvent a) {
+     float tempoFactor = DM.sequencer.getTempoFactor();
+            DM.sequencer.setTempoFactor((float)(tempoFactor * .97));
+        }
+    } // close inner class
 	
 }
